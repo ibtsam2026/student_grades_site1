4262709b062930529230588d303ce7e7274db0e0
ibtsam2026 <ibtsam2026@gmail.com>
2025-09-30 01:11:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 async function fetchStudents() {
   const response = await fetch("https://script.google.com/macros/s/AKfycbwF6iPN5OeqQBKW9WLUhTi8mPDkmmXRkzjAAscFTfJljdUR-Xf0yUMh76Dc0V6E2M-qWg/exec");
   const students = await response.json();
@@ -8,15 +7,15 @@ async function fetchStudents() {
 async function searchStudent() {
   const id = document.getElementById("nationalId").value.trim();
   const students = await fetchStudents();
-  const student = students.find(s => s["السجل المدني"] == id);
+  const student = students.find(s => s["national_id"] == id);
 
   if (student) {
-    const total = Number(student["المهام الأدائية والمشاركة والتفاعل/40"]) + Number(student["تقويمات تحريرية وتطبيقات عملية/60"]);
+    const total = Number(student["performance_score_40"]) + Number(student["assessment_score_60"]);
     document.getElementById("result").innerHTML = `
-      <p><strong>الاسم:</strong> ${student["اسم الطالب"]}</p>
-      <p><strong>الفصل:</strong> ${student["الفصل"]}</p>
-      <p><strong>المهام الأدائية والمشاركة والتفاعل:</strong> ${student["المهام الأدائية والمشاركة والتفاعل/40"]} / 40</p>
-      <p><strong>التقويمات التحريرية والتطبيقات العملية:</strong> ${student["تقويمات تحريرية وتطبيقات عملية/60"]} / 60</p>
+      <p><strong>الاسم:</strong> ${student["student_name"]}</p>
+      <p><strong>الفصل:</strong> ${student["class"]}</p>
+      <p><strong>المهام الأدائية والمشاركة:</strong> ${student["performance_score_40"]} / 40</p>
+      <p><strong>التقويمات التحريرية والتطبيقات العملية:</strong> ${student["assessment_score_60"]} / 60</p>
       <p><strong>المجموع:</strong> ${total} / 100</p>
     `;
   } else {
